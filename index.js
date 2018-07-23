8bc3b4bf7b608cbc32bf093f89ac705ffb46a1d6
Yunus Eş <yunus@yunuses.com>
2018-07-23 21:35:11 +0300

    Başlangıç kodları oluşturuldu ve JSON denendi

---
+++ index.js
@@ -0,0 +1,19 @@
+var me = {
+  "name": "Yunus",
+  "surname": "Eş",
+  "pet": {
+    "animalName": "rifat",
+    "age": 3
+  },
+  "grades": [{
+    "lesson": "Math",
+    "grade": 85
+  },{
+    "lesson": "Türkçe",
+    "grade": 70
+  },12]
+}
+
+me.grades.push({"lesson":"Beden", "grade": 100});
+
+console.log(me);
